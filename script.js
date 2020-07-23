6f3baaf988e7104c67930fd9076aed5380f42e66
Damilola Sigbeku <Dsigbeku@yahoo.com>
2020-07-23 14:42:34 -0500

    made some changes to my script.js file

--- script.js
+++ script.js
@@ -1,53 +1,4 @@
-let destination = [
-   {
-      "name": "Tatooine",
-      "diameter": "10465 km",
-      "star": "Tatoo I & Tatoo II",
-      "distance": "43000 light years from galactic core",
-      "image": "https://www.nasa.gov/sites/default/files/images/587837main_Kepler16_transit_art2_full.jpg",
-      "moons": 3
-   },
-   {
-       "name": "Pern",
-       "diameter": "measurement is under dispute",
-       "star": "Alpha Sagittarius (a.k.a. Rukbat)",
-       "distance": "Varies - find a library",
-       "image": "https://www.nasa.gov/centers/langley/images/content/698148main_Brains_904_2.jpg",
-       "moons": 2
-   },
-   {
-       "name": "Saturn/Titan",
-       "diameter": "5149.5 km",
-       "star": "Sol",
-       "distance": "1.4 billion km from Earth",
-       "image": "https://solarsystem.nasa.gov/system/resources/detail_files/16278_PIA20016.jpg",
-       "moons": 0
-   },
-   {
-       "name": "Mars",
-       "diameter": "6779 km",
-       "star": "Sol",
-       "distance": "225 million km from Earth",
-       "image": "https://mars.nasa.gov/system/resources/detail_files/7808_global-color-views-mars-PIA00407-full2.jpg",
-       "moons": 2
-   },
-   {
-       "name": "K2-18b",
-       "diameter": "34500 km",
-       "star": "K2-18",
-       "distance": "110 light years from Earth",
-       "image": "https://www.nasa.gov/sites/default/files/thumbnails/image/heic1916a.jpg",
-       "moons": "unknown"
-   },
-   {
-       "name": "Jupiter/Europa",
-       "diameter": "3,121.6 km",
-       "star": "Sol",
-       "distance": "628.3 million km from Earth",
-       "image": "https://apod.nasa.gov/apod/image/1609/Europa_Galileo_960.jpg",
-       "moons": 0
-   }
-]
+
 
 // Write your JavaScript code here!
 
@@ -65,8 +16,11 @@ let destination = [
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
    let missionTarget = document.querySelector("#missionTarget");
-   let currentDestination = destination[Math.floor(Math.random() * destination.length-1)]
-   missionTarget.innerHTML = `<h2>Mission Destination</h2>
+
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      response.json().then(function(json){
+      let currentDestination = json[Math.floor(Math.random() * json.length-1)]
+      missionTarget.innerHTML = `<h2>Mission Destination</h2>
    <ol>
       <li>Name: ${currentDestination.name}</li>
       <li>Diameter: ${currentDestination.diameter}</li>
@@ -76,6 +30,9 @@ window.addEventListener("load", function() {
    </ol>
    <img src="${currentDestination.image}">`
 
+      });
+   });
+   
 
    form.addEventListener("submit", function(event){
       let pilotName = document.querySelector("#pilotName");
@@ -118,6 +75,7 @@ window.addEventListener("load", function() {
             launchStatus.innerHTML = 'Shuttle ready for launch'
             launchStatus.style.color = "green";
             faultyItems.style.visibility = "visible"
+            event.preventDefault();
          }
       }
      
